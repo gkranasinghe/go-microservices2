687e9530e4ff9cf9b8b5c124db4fd85b2cb8a872
gkranasinghe <gkranasinghe@gmail.com>
2022-11-04 15:15:00 +0530

    add handler  methods

--- data/products.go
+++ data/products.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -20,20 +21,6 @@ type Product struct {
 // Products is a collection of Product
 type Products []*Product
 
-//ToJson serializes the contents of the collection to JSON
-//NewEncoder provides better performance than json.Unmarshal as it does not have to buffer the output into an memory slice of bytes
-//this reduces allocations and overheads of the service
-
-func (p *Products) ToJson(w io.Writer) error {
-	e := json.NewEncoder(w)
-	return e.Encode(p)
-}
-
-func GetProducts() Products {
-	return productList
-}
-
-// productList is a hardcoded list of products for this example data source
 var productList = []*Product{
 	{
 		ID:          1,
@@ -53,4 +40,56 @@ var productList = []*Product{
 		CreatedOn:   time.Now().UTC().String(),
 		UpdatedOn:   time.Now().UTC().String(),
 	},
+}
+
+func GetProducts() Products {
+	return productList
+}
+
+func AddProduct(p *Product) {
+	p.ID = getNextID()
+	productList = append(productList, p)
+}
+
+func UpdateProduct(id int, p *Product) error {
+	_, pos, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+	p.ID = id
+	productList[pos] = p
+	return nil
+}
+
+var ErrProductNotFound = fmt.Errorf("Product not found")
+
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+	return nil, -1, ErrProductNotFound
+
+}
+
+// productList is a hardcoded list of products for this example data source
+
+func (p *Product) FromJSON(r io.Reader) error {
+	e := json.NewDecoder(r)
+	return e.Decode(p)
+}
+
+//ToJson serializes the contents of the collection to JSON
+//NewEncoder provides better performance than json.Unmarshal as it does not have to buffer the output into an memory slice of bytes
+//this reduces allocations and overheads of the service
+
+func (p *Products) ToJson(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(p)
+}
+
+func getNextID() int {
+	lp := productList[len(productList)-1]
+	return lp.ID + 1
 }
