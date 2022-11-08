7df11fc6cfdd6f60247944e905d800596b9f1815
gkranasinghe <gkranasinghe@gmail.com>
2022-11-08 06:04:33 +0530

    7_openapi_docs

--- handlers/products.go
+++ handlers/products.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,143 +10,47 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// Products is a http.Handler
-type Products struct {
-	l *log.Logger
-}
-
-// NewProducts creates a products handler with the given logger
-func NewProducts(l *log.Logger) *Products {
-	return &Products{l}
-}
-
-// ServeHTTP is the main entry point for the handler and satisfies the http.Handler interface
-// func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-// 	// handle the request for a list of products
-// 	if r.Method == http.MethodGet {
-// 		p.getProducts(rw, r)
-// 		return
-// 	}
-// 	if r.Method == http.MethodPost {
-// 		p.addProduct(rw, r)
-// 		return
-// 	}
-// 	if r.Method == http.MethodPut {
-// 		p.l.Println("PUT", r.URL.Path)
-// 		// expect the id in the URI
-// 		reg := regexp.MustCompile(`/([0-9]+)`)
-// 		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
-
-// 		if len(g) != 1 {
-// 			p.l.Println("Invalid URI more than one id")
-// 			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-// 			return
-// 		}
-
-// 		if len(g[0]) != 2 {
-// 			p.l.Println("Invalid URI more than one capture group")
-// 			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-// 			return
-// 		}
-
-// 		idString := g[0][1]
-// 		id, err := strconv.Atoi(idString)
-// 		if err != nil {
-// 			p.l.Println("Invalid URI unable to convert to numer", idString)
-// 			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-// 			return
-// 		}
-
-// 		p.updateProducts(id, rw, r)
-// 		return
-// 	}
-
-// 	// catch all
-// 	// if no method is satisfied return an error
-// 	rw.WriteHeader(http.StatusMethodNotAllowed)
-
-// }
-
-// getProducts returns the products from the data store
-func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
-	p.l.Println("Handle GET Products")
-
-	// fetch the products from the datastore
-	lp := data.GetProducts()
-
-	//serialize the list to JSON
-	err := lp.ToJson(rw)
-	if err != nil {
-		http.Error(rw, "Unable to marshall Json", http.StatusInternalServerError)
-	}
-
-}
-
-func (p *Products) AddProduct(rw http.ResponseWriter, r *http.Request) {
-	p.l.Println("Handle POST Product")
-	// prod := &data.Product{}
-	prod := r.Context().Value(KeyProduct{}).(data.Product)
-	// err := prod.FromJSON(r.Body)
-	// if err != nil {
-	// 	http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
-
-	// }
-	data.AddProduct(&prod)
-
-}
-
-func (p *Products) UpdateProduct(rw http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		http.Error(rw, "Unable to covert id", http.StatusBadRequest)
-		return
-	}
-	p.l.Println("Handle PUT Product", id)
-
-	prod := r.Context().Value(KeyProduct{}).(data.Product)
-	err = data.UpdateProduct(id, &prod)
-	if err == data.ErrProductNotFound {
-		http.Error(rw, "Product not found", http.StatusNotFound)
-		return
-	}
-
-	if err != nil {
-		http.Error(rw, "Product not found", http.StatusInternalServerError)
-		return
-	}
-}
-
+// KeyProduct is a key used for the Product object in the context
 type KeyProduct struct{}
 
-func (p Products) MiddlewareValidateProduct(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		prod := data.Product{}
+// Products handler for getting and updating products
+type Products struct {
+	l *log.Logger
+	v *data.Validation
+}
 
-		err := prod.FromJSON(r.Body)
-		if err != nil {
-			p.l.Println("[ERROR] deserializing product", err)
-			http.Error(rw, "Error reading product", http.StatusBadRequest)
-			return
-		}
+// NewProducts returns a new products handler with the given logger
+func NewProducts(l *log.Logger, v *data.Validation) *Products {
+	return &Products{l, v}
+}
 
-		//Validate the product
-		err = prod.Validate()
-		if err != nil {
-			p.l.Println("[ERROR] validating product", err)
-			http.Error(
-				rw,
-				fmt.Sprintf("Error validating product: %s", err),
-				http.StatusBadRequest,
-			)
-			return
-		}
+// ErrInvalidProductPath is an error message when the product path is not valid
+var ErrInvalidProductPath = fmt.Errorf("invalid path, path should be /products/[id]")
 
-		// add the product to the context
-		ctx := context.WithValue(r.Context(), KeyProduct{}, prod)
-		r = r.WithContext(ctx)
+// GenericError is a generic error message returned by a server
+type GenericError struct {
+	Message string `json:"message"`
+}
 
-		// Call the next handler, which can be another middleware in the chain, or the final handler.
-		next.ServeHTTP(rw, r)
-	})
+// ValidationError is a collection of validation error messages
+type ValidationError struct {
+	Messages []string `json:"messages"`
+}
+
+// getProductID returns the product ID from the URL
+// Panics if cannot convert the id into an integer
+// this should never happen as the router ensures that
+// this is a valid number
+func getProductID(r *http.Request) int {
+	// parse the product id from the url
+	vars := mux.Vars(r)
+
+	// convert the id into an integer and return
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		// should never happen
+		panic(err)
+	}
+
+	return id
 }
--- main.go
+++ main.go
@@ -2,44 +2,61 @@ package main
 
 import (
 	"context"
-	// "github.com/nicholasjackson/env"
-	"github.com/gkranasinghe/go-microservicecs/handlers"
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"time"
+
+	"github.com/go-openapi/runtime/middleware"
+
+	"github.com/gkranasinghe/go-microservicecs/data"
+	"github.com/gkranasinghe/go-microservicecs/handlers"
+	gohandlers "github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
-	// env.Parse()
-	// var bindAddress = env.String("BIND_ADDRESS", false, ":9090", "Bind address for the server")
-
 	l := log.New(os.Stdout, "products-api ", log.LstdFlags)
+	v := data.NewValidation()
 
 	// create the handlers
-	ph := handlers.NewProducts(l)
+	ph := handlers.NewProducts(l, v)
 
-	//create a new serve mux and register the handlers
+	// create a new serve mux and register the handlers
 	sm := mux.NewRouter()
 
-	getRouter := sm.Methods(http.MethodGet).Subrouter()
-	getRouter.HandleFunc("/", ph.GetProducts)
+	// handlers for API
+	getR := sm.Methods(http.MethodGet).Subrouter()
+	getR.HandleFunc("/products", ph.ListAll)
+	getR.HandleFunc("/products/{id:[0-9]+}", ph.ListSingle)
 
-	putRouter := sm.Methods(http.MethodPut).Subrouter()
-	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProduct)
-	putRouter.Use(ph.MiddlewareValidateProduct)
+	putR := sm.Methods(http.MethodPut).Subrouter()
+	putR.HandleFunc("/products", ph.Update)
+	putR.Use(ph.MiddlewareValidateProduct)
 
-	postRouter := sm.Methods(http.MethodPost).Subrouter()
-	postRouter.HandleFunc("/", ph.AddProduct)
-	postRouter.Use(ph.MiddlewareValidateProduct)
+	postR := sm.Methods(http.MethodPost).Subrouter()
+	postR.HandleFunc("/products", ph.Create)
+	postR.Use(ph.MiddlewareValidateProduct)
+
+	deleteR := sm.Methods(http.MethodDelete).Subrouter()
+	deleteR.HandleFunc("/products/{id:[0-9]+}", ph.Delete)
+
+	// handler for documentation
+	opts := middleware.RedocOpts{SpecURL: "/swagger.yaml"}
+	sh := middleware.Redoc(opts, nil)
+
+	getR.Handle("/docs", sh)
+	getR.Handle("/swagger.yaml", http.FileServer(http.Dir("./")))
+
+	// CORS
+	ch := gohandlers.CORS(gohandlers.AllowedOrigins([]string{"*"}))
 
 	// create a new server
 	s := http.Server{
 		Addr:         ":9090",           // configure the bind address
-		Handler:      sm,                // set the default handler
+		Handler:      ch(sm),            // set the default handler
 		ErrorLog:     l,                 // set the logger for the server
 		ReadTimeout:  5 * time.Second,   // max time to read request from the client
 		WriteTimeout: 10 * time.Second,  // max time to write response to the client
@@ -60,7 +77,7 @@ func main() {
 	// trap sigterm or interupt and gracefully shutdown the server
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
-	// signal.Notify(c, os.Kill)
+	signal.Notify(c, os.Kill)
 
 	// Block until a signal is received.
 	sig := <-c
